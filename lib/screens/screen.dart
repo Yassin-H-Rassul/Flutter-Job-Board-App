d132f75c9a044c8c2438a6291970547c8226f724
MiranAliRashid <miranrashid@outlook.com>
2022-01-27 16:42:55 +0300

    add auth service anony

--- lib/screens/screen.dart
+++ lib/screens/screen.dart
@@ -1,20 +1,15 @@
-<<<<<<< HEAD
-// class Screen extends StatefulWidget {
-//   const Screen({Key? key}) : super(key: key);
-=======
 import 'package:flutter/material.dart';
 
 class Screen extends StatefulWidget {
   const Screen({Key? key}) : super(key: key);
->>>>>>> e0e0097c1c0c49d0c6a130fb7bbb9e6c88b8b536
 
-//   @override
-//   ScreenState createState() => ScreenState();
-// }
+  @override
+  ScreenState createState() => ScreenState();
+}
 
-// class ScreenState extends State<Screen> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container();
-//   }
-// }
+class ScreenState extends State<Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/services/authService.dart
@@ -0,0 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AuthService extends ChangeNotifier {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  String? user;
+  void signInAnonymously() {
+    _firebaseAuth
+        .signInAnonymously()
+        .then((value) => {user = "anonymous", print(value.credential)});
+  }
+}
