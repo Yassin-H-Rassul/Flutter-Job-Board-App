96ef5d704b6a7bb7570802fd907bd48ccf802653
Yassin Rassul <yassinhrassul@gmail.com>
2022-01-27 16:29:58 +0300

    added a method'

--- lib/widgets/widget.dart
+++ lib/widgets/widget.dart
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 // class Widget extends StatelessWidget {
 //   const Widget({Key? key}) : super(key: key);
-=======
-import 'package:flutter/material.dart';
-
-class Widget extends StatelessWidget {
-  const Widget({Key? key}) : super(key: key);
->>>>>>> e0e0097c1c0c49d0c6a130fb7bbb9e6c88b8b536
 
 //   @override
 //   Widget build(BuildContext context) {
