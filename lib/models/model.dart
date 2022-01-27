b457e219cf5595dd3b197bd3aaa657f9779d53d6
MiranAliRashid <miranrashid@outlook.com>
2022-01-27 16:12:44 +0300

    add data model

--- lib/models/model.dart
+++ lib/models/model.dart
@@ -1,1 +1,44 @@
-class Model {}
+class ModelJob {
+  String? id;
+  String? title;
+  String? category;
+  String? companyName;
+  String? description;
+  String? hiers;
+  String? salary;
+  DateTime? validDate;
+  String? email;
+
+  ModelJob({
+    this.id,
+    this.title,
+    this.category,
+    this.companyName,
+    this.hiers,
+    this.salary,
+    this.validDate,
+    this.email,
+  });
+
+  factory ModelJob.fromMap(Map<String, dynamic> json) => ModelJob(
+        id: json["id"],
+        title: json["title"],
+        category: json["category"],
+        companyName: json["companyName"],
+        hiers: json["hiers"],
+        salary: json["salary"],
+        validDate: json["validDate"],
+        email: json["email"],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "id": id,
+        "title": title,
+        "category": category,
+        "companyName": email,
+        "hiers": hiers,
+        "salary": salary,
+        "validDate": validDate,
+        "email": email,
+      };
+}
