c96770a6b7078c70bedc0b25b6dfa49ec0b8c46f
Yassin Rassul <yassinhrassul@gmail.com>
2022-01-27 21:25:28 +0300

    added .

--- lib/services/firebase_helper.dart
+++ lib/services/firebase_helper.dart
@@ -8,5 +8,51 @@ class FirebaseHelper {
     _firebaseFirestore.collection('jobs').add(theModelJob.toMap());
   }
 
-  //Stream getJobs() {}
+  Stream<List<ModelJob>> getStreamOfJobs() {
+    return _firebaseFirestore.collection('jobs').snapshots().map(
+          (v) => v.docs
+              .map(
+                (e) => ModelJob.fromMap(e.data()),
+              )
+              .toList(),
+        );
+  }
+
+  Stream<List<ModelJob>>? streamOfJobsWithSortedAndSearched(
+      {String? searchWord, String? sortby}) {
+    String? fieldName;
+
+    if (searchWord != null) {
+      return _firebaseFirestore
+          .collection('jobs')
+          .where('name', isEqualTo: searchWord)
+          .snapshots()
+          .map(
+            (docValue) => docValue.docs
+                .map(
+                  (e) => ModelJob.fromMap(
+                    e.data(),
+                  ),
+                )
+                .toList(),
+          );
+    } else if (sortby != null) {
+      if (sortby == 'date') fieldName = 'validDate';
+      if (sortby == 'salary') fieldName = 'salary';
+
+      return _firebaseFirestore
+          .collection('jobs')
+          .orderBy(fieldName!)
+          .snapshots()
+          .map(
+            (docValue) => docValue.docs
+                .map(
+                  (e) => ModelJob.fromMap(
+                    e.data(),
+                  ),
+                )
+                .toList(),
+          );
+    }
+  }
 }
