266cd31c360ce89cfe2cc51cdc8df2449fd03e93
Bawar Khalid <bawarkhalid912@gmail.com>
2022-01-28 20:03:57 +0300

    commit

--- lib/screens/screen.dart
+++ lib/screens/screen.dart
@@ -1,10 +1,12 @@
+import 'package:flutter/material.dart';
+
 class Screen extends StatefulWidget {
   const Screen({Key? key}) : super(key: key);
 
   @override
   ScreenState createState() => ScreenState();
 }
-
+// slaw awa testa
 class ScreenState extends State<Screen> {
   @override
   Widget build(BuildContext context) {
