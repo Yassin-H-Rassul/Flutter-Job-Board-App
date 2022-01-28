477878ffefbab20365581198792d72d3175f49f0
Bawar Khalid <bawarkhalid912@gmail.com>
2022-01-28 20:12:48 +0300

    initial commit

--- lib/screens/screen.dart
+++ lib/screens/screen.dart
@@ -1,15 +1,195 @@
 import 'package:flutter/material.dart';
 
-class Screen extends StatefulWidget {
-  const Screen({Key? key}) : super(key: key);
+class ThirdScreen extends StatefulWidget {
+  const ThirdScreen({ Key? key }) : super(key: key);
 
   @override
-  ScreenState createState() => ScreenState();
+  State<ThirdScreen> createState() => _ThirdScreenState();
 }
-// slaw awa testa
-class ScreenState extends State<Screen> {
+
+class _ThirdScreenState extends State<ThirdScreen> {
+  
+  DateTime _dateTime = DateTime.now();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return   Scaffold(
+  appBar: AppBar(
+    title: Text('Add a new job'),
+  ),
+  body: Column(
+    children: [
+      Container(
+        width: 350,
+        child: Card(
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  labelText: '*Job Title',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  )
+                ),
+              )
+            ]
+          )
+         // color: Colors.grey,
+        ),
+      ),
+
+      Container(
+         width: 350,
+        child: Card(
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  labelText: '*Category',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  )
+                ),
+              )
+            ]
+          )
+         // color: Colors.grey,
+        ),
+      ),
+       Container(
+         width: 350,
+        child: Card(
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  labelText: '*Company name',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  )
+                ),
+              )
+            ]
+          )
+         // color: Colors.grey,
+        ),
+       ),
+      Container(
+        width: 350,
+        child: Card(
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Job description',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  )
+                ),
+              )
+            ]
+          )
+         // color: Colors.grey,
+        ),
+      ),
+       Container(
+         width: 350,
+        child: Card(
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'How many Hires',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  )
+                ),
+              )
+            ]
+          )
+        
+        ),
+       ),
+       Container(
+         width: 350,
+        child: Card(
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Salary estimation',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  )
+                ),
+              )
+            ]
+          )
+         
+        ),
+       ),
+
+       Container(
+         width: 250,
+         margin: EdgeInsets.fromLTRB(0, 0, 98, 0),
+         child: InkWell(
+           onTap: (){
+             onPressed: () async{
+
+           await showDatePicker (
+              context: context, 
+              initialDate: _dateTime,
+              firstDate: DateTime(2000),
+              lastDate: DateTime(2050),
+            ).then((date)
+            {
+              setState(){
+                _dateTime = date!;
+              }
+            }
+            );
+          };
+           },
+           child: Card(
+            child: ListTile(
+              title: Text('valid till date'),
+            ),
+            color: Colors.grey,
+                 
+
+               ),
+         ),
+       ),
+
+       Container(
+         width: 350,
+        child: Card(
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Send CV to email',
+                  
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  )
+                ),
+              )
+            ]
+          )
+         // color: Colors.grey,
+        ),
+       ),
+
+      Card(
+        margin: EdgeInsets.fromLTRB(10, 150, 10, 0),
+        color: Colors.greenAccent,
+        child: ListTile(
+          title: Text('Add New Job'),
+        ),
+      ),
+    ],
+    
+  ),
+);
   }
 }
